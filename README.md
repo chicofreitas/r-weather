81e684cc5aa9f75a9359419b042a946f8a943601
Chico Freitas <freitas.phys84@gmail.com>
2022-07-11 19:37:59 -0300

    calling the 3 hour forecast: 5 days API from OpenWeather

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # R Weather
+What is this?
 
+## About the source
 Base on the six best weather apis
 https://nordicapis.com/6-best-free-and-paid-weather-apis/
 
-I'm using the OpenWeather API https://nordicapis.com/6-best-free-and-paid-weather-apis/
+I'm using the free [3 hour forecast: 5 days](https://openweathermap.org/forecast5) API from [OpenWeather](https://openweathermap.org/).
+## About location
+
+The time forecasts are from _Teresina_, estate of _Piauí_ located in _Brazil_. using the coordinates _-5.091003859078568_ for latitude and _-42.78639675351683_ for longitude.
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React from "react";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+const baseURL = "https://api.openweathermap.org/data/2.5/forecast?lat=-5.091003859078568&lon=-42.78639675351683&units=metric&appid=d04dce313275a411851e5bc70fe91408"
 
 export default function App() {
 
@@ -14,11 +14,18 @@ export default function App() {
   }, []);
 
   if(!post) return null;
-
+  console.log(post.list[0].weather[0].main);
   return (
     <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
+      <h1>{post.city.name}</h1>
+      <p>{post.list[0].weather[0].main}</p>
+      <p>{post.list[0].weather[0].description}</p>
+      <h2>Temperature:</h2>
+      <strong>{post.list[0].main.temp} °C</strong>
+      <p>Min: {post.list[0].main.temp_min} °C</p>
+      <p>Max: {post.list[0].main.temp_max} °C</p>
+
+      <h1>Velocidade do vento</h1>
     </div>
   )
 }
